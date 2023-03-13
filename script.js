87568c60814ebf9542acbc61f26b27ef0d7ff748
Conzer <119615637+Contentzer0@users.noreply.github.com>
2023-03-13 15:38:48 -0400

    please kill me

--- script.js
+++ script.js
@@ -29,30 +29,50 @@ class Deck {
 
 class GameOfWar {
     constructor () {
-        this.p1 = []
-        this.p2 = []
-        this.hand1 = []
-        this.hand2 = []
+        this.p1 = [];
+        this.p2 = [];
+        this.hand1 = [];
+        this.hand2 = [];
         this.half()
         this.draw()
         this.battle()
     }
  
     half() {
-        let deck = new Deck()
-        this.p1.push(...deck.cards.splice(0, 26))
-        this.p2.push(...deck.cards.splice(0))
+        let deck = new Deck();
+        this.p1.push(...deck.cards.splice(0, 26));
+        this.p2.push(...deck.cards.splice(0));
     }
     draw() {
-        this.hand1 = this.p1.shift()
-        this.hand2 = this.p2.shift()
+        this.hand1.push(this.p1.shift());
+        // console.log(this.hand1)
+        this.hand2.push(this.p2.shift());
+        // console.log(this.hand2)
+    }
+    war() {
+        let warHand1 = this.p1.splice[0, 4]
+        let warHand2 = this.p2.splice[0, 4]
+        this.hand1.push(...warHand1)    
+        this.hand2.push(...warHand2)    
+        if (this.hand1[this.hand1.length-1].value > this.hand2[this.hand2.length-1].value) {
+            console.log("p1 wins the war") 
+            this.p1.push(...this.hand1, ...this.hand2)
+        } else if (this.hand2[this.hand].value > this.hand1[4].value) {  
+            console.log("p2 wins the war") 
+            this.p2.push(...this.hand1, ...this.hand2)
+        } else {
+
+        }
     }
     battle() {
-        if (this.hand1.value > this.hand2.value) {
-            return (console.log("p1 wins round!") + this.p1.push(this.hand1, this.hand2))
-        } else if (this.hand2.value > this.hand1.value) {
-            return (console.log("p2 wins round!") + this.p2.push(this.hand1, this.hand2))
+        if (this.hand1[0].value > this.hand2[0].value) {
+            return (console.log("p1 wins round!") + this.p1.push(...this.hand1, ...this.hand2))
+        } else if (this.hand2[0].value > this.hand1[0].value) {
+            return (console.log("p2 wins round!") + this.p2.push(...this.hand1, ...this.hand2))
+        } else {
+            return this.war()
         }
+            
     }
 
 }
