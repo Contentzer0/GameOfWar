f36d2cafd6dac5e4bd68a6bf7c4feae23c23cd58
Conzer <119615637+Contentzer0@users.noreply.github.com>
2023-03-13 12:54:41 -0400

    fist commit

--- script.js
+++ script.js
@@ -35,16 +35,24 @@ class GameOfWar {
         this.hand2 = []
         this.half()
         this.draw()
+        this.battle()
     }
-
+ 
     half() {
         let deck = new Deck()
         this.p1.push(...deck.cards.splice(0, 26))
         this.p2.push(...deck.cards.splice(0))
     }
     draw() {
-        this.hand1.pop(this.p1)
-        this.hand2.pop(this.p2)
+        this.hand1 = this.p1.shift()
+        this.hand2 = this.p2.shift()
+    }
+    battle() {
+        if (this.hand1.value > this.hand2.value) {
+            return (console.log("p1 wins round!") + this.p1.push(this.hand1, this.hand2))
+        } else if (this.hand2.value > this.hand1.value) {
+            return (console.log("p2 wins round!") + this.p2.push(this.hand1, this.hand2))
+        }
     }
 
 }
